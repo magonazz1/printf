ff4f127e1d40b84f371a552ea65aac190cdee25b
magonazz1 <martinmagonazz@outlook.com>
2023-04-24 17:32:57 -0700

    test

--- _printf.c
+++ _printf.c
@@ -1,15 +1,63 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
+
 /**
- * _printf - function that produces output according to a format
- *
- * @format: string format to be printed
- *
- * @...: variable number of arguments
- *
- * Return: The printed string
- */
+* print_integer - prints an integer to the standard output stream
+*
+* @n: the integer to print
+*
+* Return: the number of characters printed
+*/
+int print_integer(int n)
+{
+        int count = 0;
+
+        if (n == 0)
+        {
+                putchar('0');
+                count++;
+        }
+        else if (n < 0)
+        {
+                putchar('-');
+                count++;
+                n = -n;
+        }
+        if (n / 10)
+        {
+                count = count + print_integer(n / 10);
+        }
+        putchar(n % 10 + '0');
+        count++;
+        return (count);
+}
+
+/**
+* print_binary - prints an unsigned integer in binary format
+*
+* @n: the unsigned integer to print
+*
+* Return: the number of characters printed
+*/
+int print_binary(unsigned int n)
+{
+        int count = 0;
+
+        if (n > 1)
+        {
+                        count = count + print_binary(n >> 1);
+        }
+        count = count + putchar((n & 1) + '0');
+        return (count);
+}
+/**
+* _printf - prints a formatted string to the standard output stream
+*
+* @format: a pointer to a string containing zero or more format specifiers
+*
+* Return: the number of characters printed
+*/
 int _printf(const char *format, ...)
 {
 	int count = 0;
@@ -23,23 +71,34 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == 'c')
 			{
-				char c = (char) va_arg(args, int);
+			char c = (char) va_arg(args, int);
 
-				putchar(c);
-				count++;
+			putchar(c);
+			count++;
 			}
 			else if (*format == 's')
 			{
 				char *s = va_arg(args, char *);
 
 				fputs(s, stdout);
-				count = count + strlen(s);
+				count += strlen(s);
+			}
+			else if (*format == 'd' || *format == 'i')
+			{
+				int num = va_arg(args, int);
+
+				count = + print_integer(num);
 			}
 			else if (*format == '%')
 			{
 				putchar('%');
 				count++;
 			}
+			else if (*format == 'b')
+			{
+				unsigned int n = va_arg(args, unsigned int);
+
+				count = count + print_binary(n);
 		}
 		else
 		{
--- main.h
+++ main.h
@@ -2,31 +2,18 @@
 #define MAIN_H
 
 #include <stdarg.h>
-#include <unistd.h>
-
-/**
- * struct print_func - struct to match format specifier with print function
- *
- * @specifier: format specifier
- *
- * @f: function pointer to corresponding print function
- */
-typedef struct print_func
-{
-	char *specifier;
-	int (*f)(va_list);
-} print_func_t;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int _printf(const char *format, ...);
+int _putchar(char c);
 int print_char(va_list arg);
 int print_string(va_list arg);
 int print_percent(va_list arg);
 int print_integer(va_list arg);
-int print_unsigned_integer(va_list arg);
-int print_binary(va_list arg);
-int print_octal(va_list arg);
-int print_hex(va_list arg);
-int print_hex_upper(va_list arg);
+int print_binary(unsigned int n);
+
 
 #endif /* MAIN_H */
 
