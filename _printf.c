8aaee3530537f1b0e2dc111ab7fa22f7f6a29658
magonazz1 <martinmagonazz@outlook.com>
2023-04-24 18:13:56 -0700

    test

--- _printf.c
+++ _printf.c
@@ -1,112 +1,91 @@
 #include "main.h"
-#include <stdarg.h>
-#include <stdio.h>
+
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
 
 /**
-* print_integer - prints an integer to the standard output stream
-*
-* @n: the integer to print
-*
-* Return: the number of characters printed
-*/
-int print_integer(int n)
+ * cleanup - Performs cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ *
+ * Return: void.
+ */
+void cleanup(va_list args, buffer_t *output)
 {
-        int count = 0;
-
-        if (n == 0)
-        {
-                putchar('0');
-                count++;
-        }
-        else if (n < 0)
-        {
-                putchar('-');
-                count++;
-                n = -n;
-        }
-        if (n / 10)
-        {
-                count = count + print_integer(n / 10);
-        }
-        putchar(n % 10 + '0');
-        count++;
-        return (count);
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
 }
 
 /**
-* print_binary - prints an unsigned integer in binary format
-*
-* @n: the unsigned integer to print
-*
-* Return: the number of characters printed
-*/
-int print_binary(unsigned int n)
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @args: A va_list of arguments.
+ * @output: A buffer_t struct containing a buffer.
+ * f - function
+ * buffer_t - description
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
 {
-        int count = 0;
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
 
-        if (n > 1)
-        {
-                        count = count + print_binary(n >> 1);
-        }
-        count = count + putchar((n & 1) + '0');
-        return (count);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		len = 0;
+		if (format[i] == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (format[i + tmp + 1] == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
 }
+
 /**
-* _printf - prints a formatted string to the standard output stream
-*
-* @format: a pointer to a string containing zero or more format specifiers
-*
-* Return: the number of characters printed
-*/
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
-	int count = 0;
+	buffer_t *output;
 	va_list args;
+	int ret;
 
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 	va_start(args, format);
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (*format == 'c')
-			{
-			char c = (char) va_arg(args, int);
-
-			putchar(c);
-			count++;
-			}
-			else if (*format == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				fputs(s, stdout);
-				count += strlen(s);
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int num = va_arg(args, int);
-
-				count = + print_integer(num);
-			}
-			else if (*format == '%')
-			{
-				putchar('%');
-				count++;
-			}
-			else if (*format == 'b')
-			{
-				unsigned int n = va_arg(args, unsigned int);
-
-				count = count + print_binary(n);
-		}
-		else
-		{
-			putchar(*format);
-			count++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+	ret = run_printf(format, args, output);
+	return (ret);
 }
+
